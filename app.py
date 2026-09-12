4eca496f26f786141049edab089a7f18dea21c12
Firnaeth. <gestionprojets.yt@gmail.com>
2026-09-12 17:04:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -319,7 +319,7 @@ if st.session_state.active_tab == "QUI SOMMES-NOUS":
         # Affichage de la bonne boîte d'archive selon l'année sélectionnée
         if st.session_state.Subnautica_2_story_year == "2011":
             st.markdown(
-                '<div class="subnautica-box-base"><b>📁 ARCHIVE_2011 : CRÉATION DU Blog d'Over_1</b><br><br><span style="color: #00daff; margin-right: 8px;">·</span> 📋 Blog consacré à l'actualité de la Japan Expo, du TGS, d'Epitanime et d'Animasia<br><span style="color: #00daff; margin-right: 8px;">·</span> 📋 Transition du blogs vers les réseaux sociaux</div>',
+                '<div class="subnautica-box-base"><b>📁 ARCHIVE_2011 : CRÉATION DU Blog d\'Over_1</b><br><br><span style="color: #00daff; margin-right: 8px;">·</span> 📋 Blog consacré à l'actualité de la Japan Expo, du TGS, d'Epitanime et d'Animasia<br><span style="color: #00daff; margin-right: 8px;">·</span> 📋 Transition du blogs vers les réseaux sociaux</div>',
                 unsafe_allow_html=True)
         
         if st.session_state.Subnautica_2_story_year == "2012":
